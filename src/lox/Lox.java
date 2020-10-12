9a46f5f96cd26082c730763b7f3d42bf42b015f3
Lau Siaw Sam <lausiawsam98@gmail.com>
2020-10-12 21:44:41 +0800

    Tweak Lox.report method and add error reporting method for tokens

--- src/lox/Lox.java
+++ src/lox/Lox.java
@@ -64,8 +64,16 @@ public class Lox {
        report(line, "", message);
     }
 
+    static void error(Token token, String message) {
+        if (token.type == TokenType.EOF) {
+            report(token.line, " at end", message);
+        } else {
+            report(token.line, " at '" + token.lexeme + "'", message);
+        }
+    }
+
     private static void report(int line, String where, String message) {
-        System.err.println("Error on line " + line + ": " + message);
+        System.err.println("[line " + line + "] Error" + where + ": " + message);
         hadError = true;
     }
 }
