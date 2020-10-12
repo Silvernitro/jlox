2f62ee01356a5b91385418feed2950eafa23aab3
Lau Siaw Sam <lausiawsam98@gmail.com>
2020-10-12 21:45:28 +0800

    Add comparison, term, factor, unary, primary parsers and error handling utilities

--- src/lox/Parser.java
+++ src/lox/Parser.java
@@ -3,6 +3,8 @@ package lox;
 import java.util.List;
 
 public class Parser {
+    private static class ParseError extends RuntimeException {}
+
     private final List<Token> tokens;
     private int current = 0;
 
@@ -27,8 +29,69 @@ public class Parser {
     }
 
     private Expr comparison() {
-        //TODO: implement comparision parser
-        return null;
+        Expr expr = term();
+
+        while (match(TokenType.GREATER,
+                     TokenType.GREATER_EQUAL,
+                     TokenType.LESS,
+                     TokenType.LESS_EQUAL
+                     )) {
+            Token operator = previous();
+            Expr right = term();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+
+        return expr;
+    }
+
+    private Expr term() {
+        Expr expr = factor();
+
+        while (match(TokenType.MINUS, TokenType.PLUS)) {
+            Token operator = previous();
+            Expr right = factor();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+
+        return expr;
+    }
+
+    private Expr factor() {
+        Expr expr = unary();
+
+        while (match(TokenType.SLASH, TokenType.STAR)) {
+            Token operator = previous();
+            Expr right = unary();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+
+        return expr;
+    }
+
+    private Expr unary() {
+        if (match(TokenType.BANG, TokenType.MINUS)) {
+            Token operator = previous();
+            Expr right = unary();
+            return new Expr.Unary(operator, right);
+        }
+
+        return primary();
+    }
+
+    private Expr primary() {
+        if (match(TokenType.TRUE)) return new Expr.Literal(true);
+        if (match(TokenType.FALSE)) return new Expr.Literal(false);
+        if (match(TokenType.NIL)) return new Expr.Literal(null);
+
+        if (match(TokenType.STRING, TokenType.NUMBER)) {
+            return new Expr.Literal(previous().literal);
+        }
+
+        if (match(TokenType.LEFT_PAREN)) {
+            Expr expr = expression();
+            consume(TokenType.RIGHT_PAREN, "Expect ')' after expression.");
+            return new Expr.Grouping(expr);
+        }
     }
 
     private boolean match(TokenType ...types) {
@@ -40,6 +103,16 @@ public class Parser {
         }
 
         return false;
+    }
+
+    private Token consume(TokenType type, String message) {
+        if (check(type)) return advance();
+        throw error(peek(), message);
+    }
+
+    private ParseError error(Token token, String message) {
+        Lox.error(token, message);
+        return new ParseError();
     }
 
     private boolean check(TokenType type) {
