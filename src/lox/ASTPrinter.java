4a841f9e177739e2919cdf3c65437cca35282163
Lau Siaw Sam <lausiawsam98@gmail.com>
2020-11-14 20:56:37 +0800

    Remove extraneous psvm runner in ASTPrinter

--- src/lox/ASTPrinter.java
+++ src/lox/ASTPrinter.java
@@ -39,16 +39,4 @@ public class ASTPrinter implements Expr.Visitor<String> {
 
         return builder.toString();
     }
-
-    public static void main(String[] args) {
-        Expr expression = new Expr.Binary(
-                new Expr.Unary(
-                        new Token(TokenType.MINUS, "-", null, 1),
-                        new Expr.Literal(123)),
-                new Token(TokenType.STAR, "*", null, 1),
-                new Expr.Grouping(
-                        new Expr.Literal(45.67)));
-
-        System.out.println(new ASTPrinter().print(expression));
-    }
 }
